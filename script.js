1f49b6afa97771a093d65a2267b3f722c7b6d6f6
Oğuzhan <oguzhan.umutlu123@gmail.com>
2024-07-19 19:58:17 +0300

    added unicode support and caesar

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
+const unicode = document.getElementById("unicode");
 const barcode = document.getElementById("barcode");
 const text = document.getElementById("text");
+const caesarInp = document.getElementById("caesar");
 
 const chars = ["█", "▌", "║", "│"];
-const LC = 5;
+let LC = 4;
+let caesar = 0;
 
-barcode.addEventListener("keyup", () => {
+function barcodeChanged() {
+    const limit = chars.length ** LC;
     const val = barcode.value.split("").map(i => chars.indexOf(i));
     let str = "";
     for (let i = 0; i < val.length; i += LC) {
-        const chr = parseInt(val.slice(i, i + LC).join(""), chars.length);
+        const chr = (parseInt(val.slice(i, i + LC).join(""), chars.length) - caesar + limit) % limit;
         str += String.fromCharCode(chr);
     }
     text.value = str;
-});
-text.addEventListener("keyup", () => {
+}
+
+function textChanged() {
+    const limit = chars.length ** LC;
     let code = "";
     for (let i = 0; i < text.value.length; i++) {
-        code += text.value[i].charCodeAt(0).toString(chars.length).padStart(LC, "0").split("").map(i => chars[i]).join("");
+        code += ((text.value[i].charCodeAt(0) + caesar) % limit).toString(chars.length).padStart(LC, "0").split("").map(i => chars[i]).join("");
     }
     barcode.value = code;
+}
+
+unicode.addEventListener("input", () => {
+    LC = unicode.checked ? 8 : 4;
+    textChanged();
+});
+
+barcode.addEventListener("input", barcodeChanged);
+text.addEventListener("input", textChanged);
+
+caesarInp.addEventListener("input", () => {
+    caesar = caesarInp.value
+    textChanged();
 });
